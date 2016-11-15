548c789c1c783424032b183512d1c88ea982643e
Nicolas Burroni <nmburroni@gmail.com>
2016-11-14 22:23:19 -0600

    data-map.js maps csv data and when done calls window.onDataReady. Should load that js last in the html. Moved json files to json folder, created empty counties.json file. Added csv file for first day.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 var map;
 
 function initMap() {
-		map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: 28.4717, lng: -81.3246},
-		zoom: 11
-		});
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 28.4717, lng: -81.3246},
+        zoom: 11
+    });
 }
